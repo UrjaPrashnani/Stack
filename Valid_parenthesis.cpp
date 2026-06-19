cb9fa804ba215d5fc94962908623d58761327f5e
UrjaPrashnani <urjaprashnani@gmail.com>
2026-06-19 20:33:05 +0530

    dsa

---
+++ Valid_parenthesis.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+
+            } else {
+                if (st.size()== 0) {
+                    return false;
+                }
+                if ((st.top() == '(' && s[i] == ')') ||
+                    (st.top() == '{' && s[i] == '}') ||
+                    (st.top() == '[' && s[i] == ']')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.size() == 0;
+    }
+};
+int main() {
+    Solution sol;
+    string s = "()[{}()]";
+
+    if (sol.isValid(s))
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+
+    return 0;
+}
